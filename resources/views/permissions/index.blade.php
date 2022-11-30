8895b27e338ba761c903bb73b5ab9614eee90837
adamrzky <adamrizky340@gmail.com>
2022-11-30 10:46:12 +0700

    manambah fitur manage permission

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -5,11 +5,11 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Products</h2>
+                <h2>Permission</h2>
             </div>
             <div class="pull-right">
-                @can('product-create')
-                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+                @can('permission-create')
+                <a class="btn btn-success" href="{{ route('permissions.create') }}"> Create New Product</a>
                 @endcan
             </div>
         </div>
@@ -27,35 +27,35 @@
         <tr>
             <th>No</th>
             <th>Name</th>
-            <th>Details</th>
+           
             <th width="280px">Action</th>
         </tr>
-	    @foreach ($products as $product)
+	    @foreach ($permissions as $permission)
 	    <tr>
 	        <td>{{ ++$i }}</td>
-	        <td>{{ $product->name }}</td>
-	        <td>{{ $product->detail }}</td>
+	        <td>{{ $permission->name }}</td>
+	      
 	        <td>
-                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
-                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
-                    @can('product-edit')
-                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+                <form action="{{ route('permissions.destroy',$permission->id) }}" method="POST">
+                    <a class="btn btn-info" href="{{ route('permissions.show',$permission->id) }}">Show</a>
+                    @can('permission-edit')
+                    <a class="btn btn-primary" href="{{ route('permissions.edit',$permission->id) }}">Edit</a>
                     @endcan
 
 
                     @csrf
                     @method('DELETE')
-                    @can('product-delete')
+                    @can('permission-delete')
                     <button type="submit" class="btn btn-danger">Delete</button>
                     @endcan
-    
-            <td>
+                </form>
+	        </td>
 	    </tr>
 	    @endforeach
     </table>
 
 
-    {!! $products->links() !!}
+    {!! $permissions->links() !!}
 
 
 
